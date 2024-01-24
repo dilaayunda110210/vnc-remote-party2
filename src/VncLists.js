ac7746141a88acd55237335206bb104c9ea7ed60
dilaayunda110210 <dilaayunda110210@gmail.com>
2024-01-24 14:41:40 +0000

    first commit

---
+++ src/VncLists.js
@@ -0,0 +1,16 @@
+export const vncList = [
+  { vncLink: "https://ppns5k-3000.csb.app" },
+  { vncLink: "https://lt9nsj-3000.csb.app" },
+  { vncLink: "https://ksx29c-3000.csb.app" },
+  { vncLink: "https://697wz7-3000.csb.app" },
+  { vncLink: "https://hrp4qg-3000.csb.app" },
+  { vncLink: "https://jsmp4y-3000.csb.app" },
+  { vncLink: "https://98325q-3000.csb.app" },
+  { vncLink: "https://38dc9y-3000.csb.app" },
+  { vncLink: "https://n8t2gy-3000.csb.app" },
+  { vncLink: "https://gqyxpn-3000.csb.app" },
+  { vncLink: "https://nppy9v-3000.csb.app" },
+  { vncLink: "https://t6tv6k-3000.csb.app" },
+
+  // ... (tambahkan item lainnya sesuai kebutuhan)
+];
