1632afa374d022a4f3f8e22a9fe2db2cfa720e72
dila ayunda <157418013+dilaayunda110210@users.noreply.github.com>
2024-08-17 14:07:12 +0000

    up

--- src/VncLists.js
+++ src/VncLists.js
@@ -1,16 +1,16 @@
 export const vncList = [
-  { vncLink: "https://gyjd7r-3000.csb.app" },
-  { vncLink: "https://ycm6mh-3000.csb.app" },
-  { vncLink: "https://29gg5p-3000.csb.app" },
-  { vncLink: "https://g4ddtt-3000.csb.app" },
-  { vncLink: "https://6vl53d-3000.csb.app" },
-  { vncLink: "https://6vyvpd-3000.csb.app" },
-  { vncLink: "https://vlxzd3-3000.csb.app" },
-  { vncLink: "https://fg577g-3000.csb.app" },
-  { vncLink: "https://h7nwrh-3000.csb.app" },
-  { vncLink: "https://9crh3m-3000.csb.app" },
-  { vncLink: "https://z74m86-3000.csb.app" },
-  { vncLink: "https://wz63dm-3000.csb.app" },
+  { vncLink: "https://x5hz6c-3000.csb.app" },
+  { vncLink: "https://42x2xz-3000.csb.app" },
+  { vncLink: "https://6vcr5d-3000.csb.app" },
+  { vncLink: "https://xxzlz4-3000.csb.app" },
+  { vncLink: "https://kp368k-3000.csb.app" },
+  { vncLink: "https://ykcsky-3000.csb.app" },
+  { vncLink: "https://g3dnrf-3000.csb.app" },
+  { vncLink: "https://7qtdph-3000.csb.app" },
+  { vncLink: "https://hkxw4k-3000.csb.app" },
+  { vncLink: "https://hdwjht-3000.csb.app" },
+  { vncLink: "https://mxn8n6-3000.csb.app" },
+  { vncLink: "https://nwj6zm-3000.csb.app" },
 ];
 
 // export const vncList = [
