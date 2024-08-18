6f63730ef5b637fd7aabceffb63d9f5d85a40d00
dila ayunda <157418013+dilaayunda110210@users.noreply.github.com>
2024-08-18 15:03:36 +0000

    up

--- src/VncLists.js
+++ src/VncLists.js
@@ -7,7 +7,7 @@ export const vncList = [
   { vncLink: "https://xy2tfs-3000.csb.app" },
   { vncLink: "https://f9mpxf-3000.csb.app" },
   { vncLink: "https://7qtdph-3000.csb.app" },
-  { vncLink: "https://hkxw4k-3000.csb.app" },
+  { vncLink: "https://llhxgz-3000.csb.app" },
   { vncLink: "https://vw984x-3000.csb.app" },
   { vncLink: "https://mxn8n6-3000.csb.app" },
   { vncLink: "https://nwj6zm-3000.csb.app" },
